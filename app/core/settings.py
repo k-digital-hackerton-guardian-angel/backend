dea1c23f67ee80dc2f74efd3e7ec82e49e054a9b
bamjun <khy5116@naver.com>
2024-11-17 21:34:04 +0900

    cors

--- app/core/settings.py
+++ app/core/settings.py
@@ -45,10 +45,18 @@ CUSTOM_APPS = [
     "a_crosswalk",
 ]
 
+CUSTOM_INSTALLED_APPS = [
+    "ninja",
+    "ninja_auth",
+    "corsheaders",
+]
+
+
 INSTALLED_APPS = DEFAULT_APPS + CUSTOM_APPS
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -57,6 +65,9 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 ROOT_URLCONF = "core.urls"
 
