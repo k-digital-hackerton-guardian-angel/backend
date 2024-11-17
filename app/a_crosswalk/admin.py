0958a056b9c7bc96948026ebe22fc21e7a3da879
bamjun <khy5116@naver.com>
2024-11-17 19:32:54 +0900

    admin setting crosswalk crossmanage

--- app/a_crosswalk/admin.py
+++ app/a_crosswalk/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Crosswalk, CrosswalkManagement
 # Register your models here.
+
+
+
+
+admin.site.register(Crosswalk)
+admin.site.register(CrosswalkManagement)
